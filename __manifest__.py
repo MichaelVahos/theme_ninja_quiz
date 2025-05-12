e2ef33649c2f83f24b37e4ffe799768d59ec2b38
MichaelVahos <michaelvahos2002@gmail.com>
2025-05-12 11:29:27 +0200

    vistas funcionando

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "Theme Ninja Quiz",
+    "description": "Un tema parecido a Kahoot",
+    "category": "Theme/Creative",
+    "version": "1.0",
+    "author": "Michael Vahos",
+    "license": "LGPL-3",
+    "depends": ["web", "website", "survey"],
+    "data": [
+        "views/kahoot_page.xml",
+        "views/kahoot_play.xml"
+    ],
+    "assets": {
+        "web.assets_frontend": [
+            "theme_ninja_quiz/static/src/css/kahoot_theme.css",
+            "theme_ninja_quiz/static/src/css/theme.css"
+        ]
+    },
+    "installable": True,
+    "application": False,
+    "auto_install": False
+}
---
+++ controllers/kahoot_game_controller.py
@@ -0,0 +1,41 @@
+from odoo import http
+from odoo.http import request
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class KahootGameController(http.Controller):
+
+    @http.route('/kahoot', type='http', auth='public', website=True)
+    def kahoot_page(self, **kw):
+        _logger.info("🔹 Entrando en /kahoot correctamente")
+        return request.render('theme_ninja_quiz.kahoot_page', {})
+
+    @http.route('/kahoot/join', type='http', auth='public', methods=['POST'], website=True, csrf=False)
+    def kahoot_join(self, **post):
+        pin = post.get('pin')
+        try:
+            pin = int(pin)
+        except (ValueError, TypeError):
+            return request.render('theme_ninja_quiz.kahoot_page', {
+                'error': 'El PIN debe ser un número.'
+            })
+
+        game = request.env['survey.game.session'].sudo().search([('id', '=', pin)], limit=1)
+
+        if game:
+            return request.redirect(f'/kahoot/play/{game.id}')
+        else:
+            return request.render('theme_ninja_quiz.kahoot_page', {
+                'error': 'PIN no válido. Intenta de nuevo.'
+            })
+
+    @http.route('/kahoot/play/<int:game_id>', type='http', auth='public', website=True)
+    def kahoot_play(self, game_id):
+        game = request.env['survey.game.session'].sudo().browse(game_id)
+        if not game.exists():
+            return request.not_found()
+        return request.render('theme_ninja_quiz.kahoot_play_page', {
+            'game': game,
+        })
+
