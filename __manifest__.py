e5ef4f1553f359e4e79e41c0aebf470797f28d35
MichaelVahos <michaelvahos2002@gmail.com>
2025-05-19 19:50:09 +0200

    reestructurando

--- __manifest__.py
+++ __manifest__.py
@@ -1,20 +1,13 @@
 {
-    "name": "Theme Ninja Quiz",
-    "description": "Un tema parecido a Kahoot",
-    "category": "Theme/Creative",
-    "version": "1.0",
-    "author": "Michael Vahos",
-    "license": "LGPL-3",
-    "depends": ["web", "website", "survey"],
-    "data": [
-        "views/kahoot_page.xml",
-        "views/kahoot_play.xml",
-        "views/kahoot_waiting_page.xml",
+    'name': 'Theme Ninja Quiz',
+    'version': '1.0',
+    'author': 'Michael Vahos',
+    'category': 'Theme',
+    'license': 'LGPL-3',
+    'depends': ['website', 'ninja_quiz'],
+    'data': [
+        'views/kahoot_page.xml',
     ],
-    "assets": {
-        "web.assets_frontend": [
-            "theme_ninja_quiz/static/src/css/kahoot_theme.css",
-            "theme_ninja_quiz/static/src/css/theme.css",
-            'theme_ninja_quiz/static/src/js/kahoot.js'
-                ],
+    'installable': True,
+    'auto_install': False,
 }
--- controllers/kahoot_game_controller.py
+++ controllers/kahoot_game_controller.py
@@ -3,15 +3,20 @@ from odoo.http import request
 
 class KahootGameController(http.Controller):
 
-    @http.route('/kahoot', type='http', auth='public', website=True)
-    def kahoot_participant(self, **kwargs):
-        # Generar un user_input si no existe
-        survey = request.env['survey.survey'].sudo().search([], limit=1)
+    @http.route('/kahoot/<int:survey_id>', type='http', auth='public', website=True)
+    def kahoot_participant(self, survey_id, **kwargs):
+        # Buscar la encuesta por ID
+        survey = request.env['survey.survey'].sudo().browse(survey_id)
+        if not survey.exists():
+            return request.not_found()
+
+        # Crear user_input para el participante
         user_input = request.env['survey.user_input'].sudo().create({
             'survey_id': survey.id,
             'partner_id': request.env.user.partner_id.id,
             'state': 'new',
         })
+
         return request.render('theme_ninja_quiz.kahoot_page', {
             'survey_id': survey.id,
             'input_id': user_input.id,
